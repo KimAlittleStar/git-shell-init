fdd7f7e9c8999434c6ad28634e0e9a2580e89d2f
KimAlittleStar <kim_alittlestar@qq.com>
2019-12-14 08:13:43 +0800

    修改在更改其他用户时并没有做到修改脚本的bug

--- configure.sh
+++ configure.sh
@@ -4,21 +4,23 @@ HOMEPATH=
 FILEPATH=$2
 echo "usr = $UER"
 echo "filepath = $2"
-if [ -n "$UER" ] ; then
-echo "no zero uer = $UER"
-HOMEPATH=/home/$UER/
+if [ -n "$UER" ]; then
+    echo "no zero uer = $UER"
+    HOMEPATH=/home/$UER/
 else
-echo "zero"
-HOMEPATH=~/
+    echo "zero"
+    HOMEPATH=~/
 fi
 cp -rf ./git-shell-commands $HOMEPATH
+
 # FILEPATH 确定了你需要将git仓库建立到那个文件夹下
-if [ -n "$FILEPATH" ] ; then
-echo "no zero FILEPATH = $FILEPATH"
+if [ -n "$FILEPATH" ]; then
+    echo "no zero FILEPATH = $FILEPATH"
 else
-echo "zero"
-FILEPATH=~/
+    echo "zero"
+    FILEPATH=~/
 fi
-echo "#!/bin/sh" > ./git-shell-commands/list                                                    #将list文件替换为 '#!/bin/sh'
-echo "find $FILEPATH/ -maxdepth 3 -type d -name \"[\s\S]*.git\"" >> ./git-shell-commands/list   #将list文件追加内容
-sed -i '3c\FILEPATH='$FILEPATH ./git-shell-commands/init                                        #将init中的文本行替换
+
+echo "#!/bin/sh" >$HOMEPATH/git-shell-commands/list                                                  #将list文件替换为 '#!/bin/sh'
+echo "find $FILEPATH/ -maxdepth 3 -type d -name \"[\s\S]*.git\"" >>$HOMEPATH/git-shell-commands/list #将list文件追加内容
+sed -i '3c\FILEPATH='$FILEPATH $HOMEPATH/git-shell-commands/init                                     #将init中的文本行替换
